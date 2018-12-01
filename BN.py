b60348c0ab8274fc45126101881be4dc4304e3c2
Daniel Fernandes <dani.s-fernandes@hotmail.com>
2018-12-01 17:36:09 +0000

    Ficheiros base - Redes Bayesianas

---
+++ BN.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 15 15:51:49 2018
+
+@author: mlopes
+"""
+
+
+
+class Node():
+    def __init__(self, prob, parents = []):
+        pass
+    
+    def computeProb(self, evid):
+        pass
+
+        return 0
+    
+class BN():
+    def __init__(self, gra, prob):
+        pass
+
+    def computePostProb(self, evid):
+        pass
+               
+        return 0
+        
+        
+    def computeJointProb(self, evid):
+        pass
+        
+        return 0
