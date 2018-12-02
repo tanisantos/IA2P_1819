ad90a8ae64166c320ee0faac6b1cf53172e91f17
Daniel Fernandes <dani.s-fernandes@hotmail.com>
2018-12-02 21:49:17 +0000

    pouca coisa, alguns comentário, gitignore

--- BN.py
+++ BN.py
@@ -9,24 +9,35 @@ Created on Mon Oct 15 15:51:49 2018
 
 class Node():
     def __init__(self, prob, parents = []):
-        pass
+        self.prob = prob          #np.array len(prob)==len(parents)
+        self.parents = parents    #lista
     
     def computeProb(self, evid):
+    ''' calculo prob condicionada
+        evid - tuplo c/ probs de tds os nodes da BN
+        retorna tuplo(?) com duas prob (False e True)'''
         pass
 
         return 0
     
 class BN():
     def __init__(self, gra, prob):
-        pass
+        self.gra = gra            #lista de listas com pais do indice
+        self.prob = prob          #lista de Nodes
 
     def computePostProb(self, evid):
+    ''' não sei
+        evid - tuplo c/ probs de tds os nodes da BN
+        retorna um valor'''
         pass
                
         return 0
         
         
-    def computeJointProb(self, evid):
+    def computeJointProb(self, evid): 
+    ''' calcula prob conjunta (tabela)
+        evid - tuplo c/ probs de tds os nodes da BN
+        retorna lista/tuplo com... (am confusion)'''
         pass
         
         return 0
